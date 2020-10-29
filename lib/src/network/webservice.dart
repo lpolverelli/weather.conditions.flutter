0558da8a26b6a98271faef879eddfd11ad70a0b7
Luca Polverelli <lucarex77@gmail.com>
2020-10-29 12:24:55 +0100

    Created Webservice and Webservice Test.

--- lib/src/network/webservice.dart
+++ lib/src/network/webservice.dart
@@ -1,25 +1,67 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:weather_conditions_flutter/src/models/city.dart';
+import 'package:weather_conditions_flutter/src/models/coordinates.dart';
+import 'package:weather_conditions_flutter/src/models/day_weather.dart';
+import 'package:weather_conditions_flutter/src/models/three_hour_weather.dart';
 
-class Webservice {
+final _url = 'api.openweathermap.org/data/2.5/forecast?';
+final _apiKey = '';
 
-  final url = 'api.openweathermap.org/data/2.5/forecast?';
-  final apiKey = '';
+Future<List<DayWeather>> fetchWeatherDataByCity(String city, http.Client client) async {
+  final responseBody = await _fetchData('${_url}q=$city&appid=$_apiKey', client);
 
-  Future<String> fetchWeatherDataByCity(String city, http.Client client) async {
-    return _fetchData('${url}q=$city&appid=$apiKey', client);
+  return _parseDayWeather(responseBody);
+}
+
+Future<List<DayWeather>> fetchWeatherDataByPosition(Coordinates coordinates, http.Client client) async {
+  final responseBody = await _fetchData('${_url}lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=$_apiKey', client);
+
+  return _parseDayWeather(responseBody);
+}
+
+Future<String> _fetchData(String url, http.Client client) async {
+  final response = await client.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  } else {
+    throw Exception('Failed to get the resources');
+  }
+}
+
+List<DayWeather> _parseDayWeather(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<String, dynamic>();
+  final city = City.fromJson(parsed['city']);
+  final list = parsed['list'];
+  var threeHourWeatherList = list.map<ThreeHourWeather>((json) => ThreeHourWeather.fromJson(json)).toList();
+  threeHourWeatherList.sort((ThreeHourWeather a, ThreeHourWeather b) => a.date.millisecondsSinceEpoch.compareTo(b.date.millisecondsSinceEpoch));
+  final groupedThreeHourWeatherList = _groupThreeHourWeatherByDate(threeHourWeatherList);
+  var dayWeatherList = List<DayWeather>();
+  for(var element in groupedThreeHourWeatherList){
+    dayWeatherList.add(DayWeather(city, element.first.date, element));
   }
 
-  Future<String> fetchWeatherDataByPosition(double latitude, double longitude, http.Client client) async {
-    return _fetchData('${url}lat=$latitude&lon=$longitude&appid=$apiKey', client);
-  }
+  return dayWeatherList;
+}
 
-  Future<String> _fetchData(String url, http.Client client) async {
-    final response = await client.get(url);
+List<List<ThreeHourWeather>> _groupThreeHourWeatherByDate(List<ThreeHourWeather> threeHourWeatherList){
+  var result = List<List<ThreeHourWeather>>();
+  var currentDate = threeHourWeatherList.first.date;
+  var currentDayList = List<ThreeHourWeather>();
 
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      throw Exception('Failed to get the resources');
+  for(var element in threeHourWeatherList){
+    if(element.date.year > currentDate.year ||
+        element.date.month > currentDate.month ||
+        element.date.day > currentDate.day){
+      result.add(List<ThreeHourWeather>.from(currentDayList));
+      currentDate = element.date;
+      currentDayList.clear();
     }
+    currentDayList.add(element);
   }
+  result.add(List<ThreeHourWeather>.from(currentDayList));
+
+  return result;
 }
--- test/webservice_test.dart
+++ test/webservice_test.dart
@@ -1,6 +1,8 @@
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
 import 'package:test/test.dart';
+import 'package:weather_conditions_flutter/src/models/coordinates.dart';
+import 'package:weather_conditions_flutter/src/models/day_weather.dart';
 import 'package:weather_conditions_flutter/src/network/webservice.dart';
 
 class MockClient extends Mock implements http.Client {}
@@ -8,36 +10,127 @@ class MockClient extends Mock implements http.Client {}
 main() {
 
   final url = 'api.openweathermap.org/data/2.5/forecast?';
-  final city = 'London';
-  final latitude = 34.257;
-  final longitude = -85.1647;
-  final webservice = Webservice();
+  final city = 'Rome';
   final client = MockClient();
+  final coordinates = Coordinates(latitude: 34.257, longitude: -85.1647);
+  final json =
+    '{'
+      '"cod": "200",'
+      '"message": 0,'
+      '"cnt": 40,'
+      '"list": ['
+        '{'
+          '"dt": 1603810800,'
+          '"main": {'
+            '"temp": 293.14'
+          '},'
+          '"weather": ['
+              '{'
+                '"id": 803,'
+                '"main": "Clouds",'
+                '"description": "broken clouds",'
+                '"icon": "04d"'
+              '}'
+          ']'
+        '},'
+        '{'
+          '"dt": 1603821600,'
+          '"main": {'
+            '"temp": 295.42'
+          '},'
+          '"weather": ['
+              '{'
+                '"id": 803,'
+                '"main": "Clouds",'
+                '"description": "broken clouds",'
+                '"icon": "04d"'
+              '}'
+          ']'
+        '},'
+        '{'
+          '"dt": 1604210400,'
+          '"main": {'
+            '"temp": 285.3'
+          '},'
+          '"weather": ['
+              '{'
+                '"id": 804,'
+                '"main": "Clouds",'
+                '"description": "overcast clouds",'
+                '"icon": "04n"'
+              '}'
+          ']'
+        '},'
+        '{'
+          '"dt": 1604221200,'
+          '"main": {'
+            '"temp": 284.89'
+          '},'
+          '"weather": ['
+            '{'
+              '"id": 804,'
+              '"main": "Clouds",'
+              '"description": "overcast clouds",'
+              '"icon": "04n"'
+            '}'
+          ']'
+        '},'
+        '{'
+          '"dt": 1604232000,'
+          '"main": {'
+            '"temp": 283.91'
+          '},'
+          '"weather": ['
+            '{'
+              '"id": 804,'
+              '"main": "Clouds",'
+              '"description": "overcast clouds",'
+              '"icon": "04n"'
+            '}'
+          ']'
+        '}'
+      '],'
+      '"city": {'
+        '"id": 4219762,'
+        '"name": "Rome",'
+        '"coord": {'
+          '"lat": 34.257,'
+          '"lon": -85.1647'
+        '},'
+        '"country": "US"'
+      '}'
+    '}';
 
   group('Fetch test', () {
 
     test('Given in input a city, returns a string if the http call completes successfully', () async {
-      when(client.get('${url}q=$city&appid=')).thenAnswer((_) async => http.Response('{"test": "Test"}', 200));
+      when(client.get('${url}q=$city&appid=')).thenAnswer((_) async => http.Response(json, 200));
 
-      expect(await webservice.fetchWeatherDataByCity('London', client), isA<String>());
+      final result = await fetchWeatherDataByCity(city, client);
+
+      expect(result, isA<List<DayWeather>>());
+      expect(result.length, 2);
     });
     
     test('Given in input a city, throws an exception if the http call completes with an error', () {
       when(client.get('${url}q=$city&appid=')).thenAnswer((_) async => http.Response('Not found', 404));
 
-      expect(webservice.fetchWeatherDataByCity('London', client), throwsException);
+      expect(fetchWeatherDataByCity(city, client), throwsException);
     });
 
     test('Given in input a position, returns a string if the http call completes successfully', () async {
-      when(client.get('${url}lat=$latitude&lon=$longitude&appid=')).thenAnswer((_) async => http.Response('{"test": "Test"}', 200));
+      when(client.get('${url}lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=')).thenAnswer((_) async => http.Response(json, 200));
 
-      expect(await webservice.fetchWeatherDataByPosition(latitude, longitude, client), isA<String>());
+      final result = await fetchWeatherDataByPosition(coordinates, client);
+
+      expect(result, isA<List<DayWeather>>());
+      expect(result.length, 2);
     });
 
     test('Given in input a position, throws an exception if the http call completes with an error', () {
-      when(client.get('${url}lat=$latitude&lon=$longitude&appid=')).thenAnswer((_) async => http.Response('Not found', 404));
+      when(client.get('${url}lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=')).thenAnswer((_) async => http.Response('Not found', 404));
 
-      expect(webservice.fetchWeatherDataByPosition(latitude, longitude, client), throwsException);
+      expect(fetchWeatherDataByPosition(coordinates, client), throwsException);
     });
     
   });
